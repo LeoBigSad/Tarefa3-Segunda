498a443a8ada1547e18b6c320059f2e6660ef4be
LeoBigSad <85249134+LeoBigSad@users.noreply.github.com>
2025-02-20 14:19:23 -0300

    Add files via upload

---
+++ Tarefa3-segunda/Controllers/PersonController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tarefa3.Domain.Interfaces.Service;
+
+namespace Tarefa3.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController(IPersonService _personService) : ControllerBase
+    {
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _personService.GetAll());
+        }
+    }
+}
---
+++ Tarefa3-segunda/Program.cs
@@ -0,0 +1,26 @@
+using Tarefa3.Application.Services;
+using Tarefa3.Domain.Interfaces.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IPersonService, PersonService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
